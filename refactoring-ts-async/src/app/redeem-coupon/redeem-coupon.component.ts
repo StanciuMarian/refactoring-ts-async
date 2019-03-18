af5fd921765120e100d97cdcc9d5fa067c41c314
Stanciu Marian Madalin <Marian.Madalin.Stanciu@ibm.com>
2019-03-18 20:45:14 +0200

    final clean-up + ugly version

--- refactoring-ts-async/src/app/redeem-coupon/redeem-coupon.component.ts
+++ refactoring-ts-async/src/app/redeem-coupon/redeem-coupon.component.ts
@@ -28,8 +28,36 @@ export class RedeemCouponComponent {
   constructor(private api: AppApi,
             private userApi: UserApi,
             private toastr: Toastr) {}
+  //poate putem porni de la asta? 
+  //aplicatia functioneaza si daca scrii asa :)
+  //nu e la fel de rau ca si cu callback-uri dar tot e foarte urat codul 
+  // ngOnInit() {
+  //   let promise1 = this.userApi.getCurrentUser().toPromise();
+  //   let promise2 = this.api.getAllCountries().toPromise();
+  //   Promise.all([promise1, promise2])
+  //     .then(data => {
+  //       this.countries = data[1];
+  //       this.selectedCountryId = data[0].countryId;
+  //       this.api.getCitiesByCountry(this.selectedCountryId).toPromise()
+  //         .then(cities => {
+  //             this.cities = cities;
+  //             this.selectedCityId = data[0].cityId; 
+  //             this.api.getStoresByCity(this.selectedCityId).toPromise()
+  //               .then(stores => {
+  //                 this.stores = stores;
+  //                 this.coupon.storeId = stores[0].id;
+  //               })
+  //               .catch(() => console.log('error on third request'))
+  //         })
+  //         .catch(() => console.log('error on second request'))
+  //     })
+  //     .catch(() => console.log('error on first request'))
+  // } //am putea sa bagam interceptorul ca mecanism de tratare a erorilor in angular si cum sa scapi de catch-urile alea
 
   ngOnInit(): void {
+    //poate o putin fortat forkJoin-ul aici
+    //orasele oricum nu sunt initializate decat cand se incarca userul 
+    //ai putea incarca userul si apoi sa iei si orasele, nu trebuie sa le iei simultan
     forkJoin(this.userApi.getCurrentUser(), this.api.getAllCountries()).subscribe(data => {
       this.countries = data[1];   
       this.initCurrentUser(data[0]);
@@ -39,6 +67,7 @@ export class RedeemCouponComponent {
 
   initCurrentUser(currentUser: UserDto) {
     //mai trebuie sa salvam userul daca il folosim doar la initializarea asta ???
+    //momentan l-am scos
     this.selectedCountryId = currentUser.countryId;
     this.selectedCityId = currentUser.cityId;
   }
@@ -70,6 +99,8 @@ export class RedeemCouponComponent {
     })
   }
 
+  //sa scoatem modala separat sa comunica cu componenta asta prin serviciu ? 
+  //nu prea e legat de asincronism, dar am putea arata ce mai poti face cu observables
   showConfirmationDialog(): void {
     this.isConfirmationDialogDisplayed = true;
   }
